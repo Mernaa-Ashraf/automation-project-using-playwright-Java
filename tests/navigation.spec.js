fbdf7dbb33ee8d5cb9ca9193110059862dd09698
merna <merna@gmail.com>
2025-11-25 16:42:41 +0200

    final script updates

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -12,39 +12,36 @@ test("footer links navigation", async ({ page }) => {
         trainerLink.click()
     ]);
 
-    expect(page.url()).toBe("https://uat-learn.eccouncil.org/instructors?logged=false");
+     expect(page.url()).toBe("https://uat-learn.eccouncil.org/instructors?logged=false");
+
     await page.goBack()
-    await expect(page).toHaveURL('https://uat-learn.eccouncil.org/?logged=false');
+    await expect(page).toHaveURL("https://uat-learn.eccouncil.org/?logged=false")
+    const privacyLink = page.locator('footer').getByRole('link', { name: 'Privacy Policy' });
 
-    const privacylink= page.locator('footer').getByRole('link', { name: 'Privacy Policy' })
-//navigating to a new tab
-   const [pagenew]= await Promise.all([
-      page.waitForEvent("popup"),
-      privacylink.click()
-    ])
-    await pagenew.waitForLoadState();
-    await expect(pagenew).toHaveURL("https://www.eccouncil.org/legal/privacy-policy/");
-
-
+//navigating to a new tab in new page 
+    const [newPage] = await Promise.all([
+     page.waitForEvent("popup"),
+      privacyLink.click()
+     ]);
+    await newPage.waitForLoadState();
+    expect(newPage).toHaveURL("https://www.eccouncil.org/legal/privacy-policy/");
 });
 
 test('navigate to login and fill credentials', async ({ page }) => {
 
-    await page.goto("https://uat-learn.eccouncil.org/?logged=false");
+    await page.goto("http://172.177.136.15/?logged=false");
     const login=page.getByRole('link', { name: 'Login' })
     await Promise.all([
-         page.waitForURL("https://uat-learn.eccouncil.org/login?logged=false"),
+         page.waitForURL("http://172.177.136.15/login?logged=false"),
          login.click()
     ])
-   await page.getByRole('textbox', { name: 'Email' }).fill("fadi_ecl_demo@eccouncil.org")
+   await page.getByRole('textbox', { name: 'Email' }).fill("Talia.Walter@gmail.com")
    await page.getByRole('textbox', { name: 'Password' }).fill("Fadysaber1!")
    const dashboard=await page.getByRole('button',{name:'login'})
    await Promise.all([
-        page.waitForURL("https://uat-learn.eccouncil.org/your-portal?logged=true"),
+        page.waitForURL("http://172.177.136.15/your-portal?logged=true"),
         dashboard.click()
    ])
-   expect(page).toHaveURL("https://uat-learn.eccouncil.org/your-portal?logged=true")
+  await expect(page).toHaveURL("http://172.177.136.15/your-portal?logged=true")
    //await page.pause();
-
-
 })
